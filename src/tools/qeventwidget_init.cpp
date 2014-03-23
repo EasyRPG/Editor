f6b4a1c6c36bf7d2f8cda438fad162d8bdeb5e20
Mariano Javier Suligoy <marianognu.easyrpg@gmail.com>
2014-03-23 09:59:01 -0300

    Update StoreTerrainID/EventID command strings.

--- src/tools/qeventwidget_init.cpp
+++ src/tools/qeventwidget_init.cpp
@@ -186,16 +186,20 @@ void QEventWidget::Init(DialogSplash *s, QProgressBar *progressBar, QLabel *labe
     reg(EnterExitVehicle, "Enter Vehicle");
 
     reg(SetVehicleLocation, "%e0.Location = %e1"
-        "@Skiff|Ship|Airship"
-        "@Map[%m2](%n3, %n4)|Map[V[%v2](V[%v3], V[%v4])");
+                            "@Skiff|Ship|Airship"
+                            "@Map[%m2](%n3, %n4)|Map[V[%v2](V[%v3], V[%v4])");
 
     reg(ChangeEventLocation, "Event[%sl0].Location = %e1"
-        "@(%n2, %n3)|(V[%v2], V[%v3])");
+                             "@(%n2, %n3)|(V[%v2], V[%v3])");
 
     reg(TradeEventLocations, "Swap (Event[%sl0].Location, Event[%sl1].Location)");
 
-    reg(StoreTerrainID, "StoreTerrainID");
-    reg(StoreEventID, "StoreEventID");
+    reg(StoreTerrainID, "V[%v3] = Tile(%e0).TerrainID"
+                        "@%n1, %n2|V[%v1], V[%v2]");
+
+    reg(StoreEventID,   "V[%v3] = Tile(%e0).EventID"
+                        "@%n1, %n2|V[%v1], V[%v2]");
+
     reg(EraseScreen, "EraseScreen");
     reg(ShowScreen, "ShowScreen");
     reg(TintScreen, "TintScreen");
