2b72c2e53fcf9a4ff430b49a4e6d4bee08791134
Mariano Javier Suligoy <marianognu.easyrpg@gmail.com>
2014-11-18 22:04:15 -0300

    Global access to Editor's settings

---
+++ src/editorsettings.cpp
@@ -0,0 +1,12 @@
+#include "editorsettings.h"
+
+#include <QApplication>
+
+EditorSettings *EditorSettings::m_instance = 0;
+
+EditorSettings::EditorSettings() :
+    QSettings(qApp->applicationDirPath()+"Easy.cfg",
+              QSettings::IniFormat,
+              qApp)
+{
+}
